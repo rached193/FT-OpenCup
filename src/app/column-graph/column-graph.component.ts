871a5d83f8f9ebc33419269d1e7da8b48711d0d7
Jorge Campo Oliván <perdiganogaming@gmail.com>
2020-04-30 11:23:09 +0200

    graficas iniciales de muestra

--- src/app/column-graph/column-graph.component.ts
+++ src/app/column-graph/column-graph.component.ts
@@ -7,21 +7,25 @@ import * as Highcharts from 'highcharts';
   styleUrls: ['./column-graph.component.scss']
 })
 export class ColumnGraphComponent implements OnInit {
-  @ViewChild('chart', { static: true }) chart: ElementRef;
+  @ViewChild('chart1', { static: true }) chart1: ElementRef;
+  @ViewChild('chart2', { static: true }) chart2: ElementRef;
+  @ViewChild('chart3', { static: true }) chart3: ElementRef;
 
-  graph: Highcharts.Chart;
+  graph1: Highcharts.Chart;
+  graph2: Highcharts.Chart;
+  graph3: Highcharts.Chart;
 
   constructor() { }
 
   ngOnInit(): void {
 
 
-    const chartOptions: Highcharts.Options = {
+    const chartOptions1: Highcharts.Options = {
       chart: {
         type: 'column'
       },
       title: {
-        text: 'Teams by Temtem'
+        text: 'Teams by Temtem (Minor Cup April)'
       },
       xAxis: {
         categories: ['Volarend', 'Kinu', 'Gyalis', 'Pigepic', 'Ukama', 'Raize', 'Tuvine', 'Valash', 'Vulcrane', 'Saipat', 'Myx', 'Kalabyss', 'Nidrasil', 'Wiplump', 'Raican', 'Mudrid'],
@@ -34,10 +38,9 @@ export class ColumnGraphComponent implements OnInit {
       },
       yAxis: {
         title: {
-          text: '% use'
+          text: ''
         },
-        min: 0,
-        max: 100
+        min: 0
       },
       legend: {
         enabled: false
@@ -45,15 +48,15 @@ export class ColumnGraphComponent implements OnInit {
       series: [{
         name: 'Teams',
         type: 'column',
-        data: [86.89, 83.61, 78.69, 70.49, 52.46, 45.90, 44.26, 42.62, 37.70, 31.15, 29.51, 24.59, 21.31, 18.03, 18.03, 13.11]
+        data: [53, 51, 48, 43, 32, 28, 27, 26, 23, 19, 18, 15, 13, 11, 11, 8]
       }]
-    }/*
-    {
+    };
+    const chartOptions2: Highcharts.Options = {
     chart: {
         type: 'column'
     },
     title: {
-        text: 'Picks&Bans by Temtem'
+        text: 'Picks/Bans by Temtem (Minor Cup April: Semifinals & Final)'
     },
     xAxis: {
         categories: ['Volarend', 'Kinu', 'Gyalis', 'Pigepic', 'Ukama', 'Tuvine', 'Valash', 'Saipat', 'Kalabyss', 'Mushook', 'Vulcrane', 'Wiplump', 'Cerneaf', 'Raican'],
@@ -66,29 +69,115 @@ export class ColumnGraphComponent implements OnInit {
     },
     yAxis: {
         title: {
-            text: '% rate'
+            text: ''
         },
-        min: 0,
-        max: 100
+        min: 0/*,
+        max: 100*/
     },
     plotOptions: {
         column: {
-            stacking: 'percent'
+            stacking: 'normal'
         }
     },
     series: [{
         name: 'Ban',
+        type: 'column',
+        color: '#FF0000',
         data: [3, 4, 11, 1, null, 1, 2, null, 4, 1, 1, null, null, null]
     }, {
         name: 'Pick',
+        color: '#00FF00',
+        type: 'column',
         data: [11, 5, 1, 11, 6, 9, 5, 7, 3, 4, 2, 2, 2, 2]
     }, {
         name: 'Out',
+        color: '#999999',
+        type: 'column',
         data: [null, 5, null, null, 6, null, 2, null, null, null, 1, null, null, null]
     }]
-}*/ ;
+};
+const chartOptions3: Highcharts.Options = {
 
-    this.graph = Highcharts.chart(this.chart.nativeElement, chartOptions)
+  title: {
+    text: 'Evolution of use (teams %)'
+  },
+
+  yAxis: {
+    title: {
+      text: ''
+    },
+    min: 0,
+        max: 100
+  },
+
+  xAxis: {
+    categories: ['Minor Cup April', 'Minor Cup May', 'Minor Cup June', 'Minor Cup July', 'Minor Cup August', 'Qualifier September', 'Minor Cup October', 'Qualifier November'],
+  },
+
+  legend: {
+    layout: 'vertical',
+    align: 'right',
+    verticalAlign: 'middle',
+    useHTML: true,
+        labelFormatter: function(){
+            return '<img src="../assets/img/'+this.name+'.png"></img>';                        
+        }
+  },
+
+  plotOptions: {
+    series: {
+      label: {
+        connectorAllowed: false
+      }
+    }
+  },
+
+  series: [{
+    name: 'Volarend',
+    type: 'line',
+    data: [86.89, null, null, null, null, null, null, null]
+  }, {
+    name: 'Kinu',
+    type: 'line',
+    data: [83.61, null, null, null, null, null, null, null]
+  }, {
+    name: 'Gyalis',
+    type: 'line',
+    data: [78.69, null, null, null, null, null, null, null]
+  }, {
+    name: 'Pigepic',
+    type: 'line',
+    data: [70.49, null, null, null, null, null, null, null]
+  }, {
+    name: 'Ukama',
+    type: 'line',
+    data: [52.46, null, null, null, null, null, null, null]
+  }, {
+    name: 'Raize',
+    type: 'line',
+    data: [45.9, null, null, null, null, null, null, null]
+  }],
+
+  responsive: {
+    rules: [{
+      condition: {
+        maxWidth: 500
+      },
+      chartOptions: {
+        legend: {
+          layout: 'horizontal',
+          align: 'center',
+          verticalAlign: 'bottom'
+        }
+      }
+    }]
+  }
+
+} ;
+
+    this.graph1 = Highcharts.chart(this.chart1.nativeElement, chartOptions1)
+    this.graph2 = Highcharts.chart(this.chart2.nativeElement, chartOptions2)
+    this.graph3 = Highcharts.chart(this.chart3.nativeElement, chartOptions3)
 
   }
 
