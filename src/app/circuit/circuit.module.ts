3ccab392b0c72fc8bc8a769da35982eac451d46b
Rached <archer193@hotmail.com>
2020-05-09 17:58:29 +0200

    Añdir side menu

--- src/app/circuit/circuit.module.ts
+++ src/app/circuit/circuit.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CircuitComponent } from './circuit.component';
 import { SharedModule } from '../shared/shared.module';
+import { SideMenuComponent } from './side-menu/side-menu.component';
 
 
 @NgModule({
-  declarations: [CircuitComponent],
+  declarations: [CircuitComponent, SideMenuComponent],
   imports: [
     CommonModule,
     SharedModule
