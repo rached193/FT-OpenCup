2e63977ad0f90f4753d1ce4f39cbc4ae3f623d88
Jorge Campo Oliván <perdiganogaming@gmail.com>
2020-10-04 01:48:22 +0200

    info actualizada, nuevos temtem

--- projects/temtem-web/src/app/overview/overview.component.ts
+++ projects/temtem-web/src/app/overview/overview.component.ts
@@ -11,7 +11,7 @@ export class OverviewComponent implements OnInit {
   id: number;
   private sub: any;
 
-  tournaments: number[] = [0, 1, 2, 3, 4, 5, 6];
+  tournaments: number[] = [0, 1, 2, 3, 4, 5, 6, 7];
 
   circuitMenu$ = this.service.getMenu();
 
