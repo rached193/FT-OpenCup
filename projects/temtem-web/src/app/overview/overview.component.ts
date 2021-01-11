949a0818a5551b1e59ad35915037150f79950a58
Jorge Campo Oliván <perdiganogaming@gmail.com>
2021-01-11 20:54:06 +0100

    torneo enero actualizado

--- projects/temtem-web/src/app/overview/overview.component.ts
+++ projects/temtem-web/src/app/overview/overview.component.ts
@@ -11,7 +11,7 @@ export class OverviewComponent implements OnInit {
   public id: number;
   private sub: any;
 
-  tournaments: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+  tournaments: number[] = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12];
 
   circuitMenu$ = this.service.getMenu();
 
